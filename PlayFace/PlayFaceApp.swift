eb9c1729b0c36f316602791704e28c56eafc886b
littleossa <littleossa@gmail.com>
2022-03-28 06:56:00 +0900

    Initial Commit

---
+++ PlayFace/PlayFaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlayFaceApp.swift
+//  PlayFace
+//
+//  Created by 平岡修 on 2022/03/28.
+//
+
+import SwiftUI
+
+@main
+struct PlayFaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

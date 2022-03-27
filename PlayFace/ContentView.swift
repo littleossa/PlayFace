e88ec0f4d12ca99c827c3cd819258298586a6186
littleossa <littleossa@gmail.com>
2022-03-28 08:52:50 +0900

    [feat] Complete PlayFaceApp

--- PlayFace/ContentView.swift
+++ PlayFace/ContentView.swift
@@ -8,10 +8,57 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
+    
+    enum FaceParts: String, Identifiable, CaseIterable {
+        
+        case leftEye
+        case rightEye
+        case nose
+        case mouth
+        
+        var id: String { rawValue }
+        
+        var imageName: String {
+            switch self {
+            case .leftEye, .rightEye:
+                return "eye"
+            case .nose:
+                return "nose"
+            case .mouth:
+                return "mouth"
+            }
+        }
+        
+        var defaultPosition: CGPoint {
+            switch self {
+            case .leftEye:
+                return CGPoint(x: 120, y: 200)
+            case .rightEye:
+                return CGPoint(x: 260, y: 200)
+            case .nose:
+                return CGPoint(x: 190, y: 300)
+            case .mouth:
+                return CGPoint(x: 190, y: 430)
+            }
+        }
     }
+    
+    var body: some View {
+        ZStack {
+            Rectangle()
+                .ignoresSafeArea()
+                .foregroundColor(.paleOrange)
+            
+            ForEach(FaceParts.allCases) {
+                DraggableImage($0.imageName,
+                               defaultLocation: $0.defaultPosition)
+            }
+        }
+    }
+}
+
+extension Color {
+    static let paleOrange = Color(red: 245 / 255, green: 218 / 255, blue: 195 / 255, opacity: 1)
 }
 
 struct ContentView_Previews: PreviewProvider {
---
+++ PlayFace/DraggableImage.swift
@@ -0,0 +1,50 @@
+//
+//  DraggableImage.swift
+//  PlayFace
+//
+//  Created by 平岡修 on 2022/03/28.
+//
+
+import SwiftUI
+
+struct DraggableImage: View {
+    
+    @State private var location: CGPoint
+    @State private var isDragging = false
+    private let imageName: String
+    
+    init(_ systemName: String,
+         defaultLocation: CGPoint) {
+        imageName = systemName
+        location = defaultLocation
+    }
+    
+    /// Drag guesure
+    var dragGesture: some Gesture {
+        DragGesture()
+            .onChanged { value in
+                self.location = value.location
+                self.isDragging = true
+            }
+            .onEnded { _ in
+                self.isDragging = false
+            }
+    }
+    
+    var body: some View {
+        Image(systemName: imageName)
+            .resizable()
+            .scaledToFit()
+            .foregroundColor(self.isDragging ? Color.blue : Color.black)
+            .frame(width: 100)
+            .position(location)
+        // Attache a drag gesture
+            .gesture(dragGesture)
+    }
+}
+
+struct DraggableImage_Previews: PreviewProvider {
+    static var previews: some View {
+        DraggableImage("eye", defaultLocation: CGPoint(x: 100, y: 100))
+    }
+}
